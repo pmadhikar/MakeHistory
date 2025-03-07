eef1902babdd5aef17ed103a9f9abb8486340983
Pranav Madhikar <pmadhikar@gmail.com>
2025-03-07 23:31:53 +0100

    Implement commit walking

--- src/git.hpp
+++ src/git.hpp
@@ -11,6 +11,8 @@ extern "C"
 #include<unordered_map>
 #include<format>
 #include<stdlib.h>
+#include<print>
+#include<functional>
 
 namespace detail
 {
@@ -39,7 +41,7 @@ namespace detail
       throw std::runtime_error(std::format("Could not open walker {}", e->message));
     }
 
-    git_revwalk_sorting(walker, GIT_SORT_TIME | GIT_SORT_TOPOLOGICAL);
+    git_revwalk_sorting(walker, GIT_SORT_TIME | GIT_SORT_TOPOLOGICAL | GIT_SORT_REVERSE);
     if (git_revwalk_push_head(walker) < 0)
     {
       const git_error *e = git_error_last();
@@ -49,13 +51,37 @@ namespace detail
     return walker;
   }
 
-  git_commit* get_commit(git_revwalk* walker, git_oid* oid, git_repository* repo)
+  std::optional<git_oid> get_next_commit_oid(git_revwalk* walker, git_repository* repo)
   {
-    git_revwalk_next(oid, walker);
+    git_oid oid{};
+
+    if (git_revwalk_next(&oid, walker) < 0){
+      return std::nullopt;
+    }
+
+    return oid;
+  }
+
+  void walk_commit_oids(git_revwalk* walker, git_repository* repo, std::function<void(git_oid)> callback)
+  {
+    for (std::optional<git_oid> maybe_oid = get_next_commit_oid(walker, repo);
+         maybe_oid.has_value();
+         maybe_oid = get_next_commit_oid(walker, repo)){
+      callback(*maybe_oid);
+    }
+  }
+
+  git_commit* get_git_commit(git_repository* repo, git_oid* oid)
+  {
     git_commit* commit;
-    git_commit_lookup(&commit, repo, oid);
+
+    if (git_commit_lookup(&commit, repo, oid) < 0){
+      const git_error *e = git_error_last();
+      throw std::runtime_error(std::format("Could not look commit up {}", e->message));
+    }
 
     return commit;
+
   }
 }
 
@@ -72,62 +98,77 @@ struct Git {
       std::println("All done. Cleaning up libgit2.");
       git_libgit2_shutdown();
     }
-    struct Repo;
-
-
-    struct RevWalker
-    {
-      std::unique_ptr<git_revwalk, decltype(&git_revwalk_free)> walker;
-      RevWalker(): walker{nullptr, git_revwalk_free} {};
-      RevWalker(Repo& repo): walker{detail::get_commit_walker(repo.repo.get()), git_revwalk_free} {};
-    };
-
-    struct Commit
-    {
-      git_oid oid;
-      std::unique_ptr<git_commit, decltype(&git_commit_free)> commit;
-      std::unique_ptr<const char> commit_msg{};
-
-      std::string sha{};
-      std::string message{};
-
-      Commit(Repo& r, RevWalker& w):
-        commit{detail::get_commit(w.walker.get(), &oid, r.repo.get()), git_commit_free}{
-
-        std::array<char, GIT_OID_HEXSZ+1> sha_array{};
-        git_oid_fmt(sha_array.data(), &oid);
-        sha_array[GIT_OID_HEXSZ] = '\0';
-
-        sha = sha_array.data();
-
-        message = git_commit_message(commit.get());
-      }
-
-    };
-
     struct Repo {
+
+      struct RevWalker
+      {
+        std::unique_ptr<git_revwalk, decltype(&git_revwalk_free)> walker;
+        RevWalker(): walker{nullptr, git_revwalk_free} {};
+        RevWalker(Repo& repo): walker{detail::get_commit_walker(repo.get()), git_revwalk_free} {};
+
+        git_revwalk* get() { return walker.get();}
+      };
+
+      struct Commit
+      {
+        git_oid oid;
+        std::unique_ptr<git_commit, decltype(&git_commit_free)> commit;
+
+        std::string sha{};
+        std::string message{};
+
+        Commit(Repo& repo_, git_oid oid_): oid{oid_}, commit{detail::get_git_commit(repo_.get(), &oid), git_commit_free}{
+          std::array<char, GIT_OID_HEXSZ+1> sha_array{};
+          git_oid_fmt(sha_array.data(), &oid);
+          sha_array[GIT_OID_HEXSZ] = '\0';
+          sha = sha_array.data();
+          message = git_commit_message(commit.get()); // leaks memory
+        }
+      };
 
       std::string_view repo_path{};
       std::unique_ptr<git_repository, decltype(&git_repository_free)> repo;
       RevWalker walker{};
+      std::string_view prefix;
+      std::vector<Commit> marked_commits{};
 
-      Repo(std::string_view repo_path_): repo_path{}, repo{detail::open_repo(repo_path_), git_repository_free}{
+      Repo(std::string_view repo_path_, std::string_view prefix_): repo_path{}, repo{detail::open_repo(repo_path_), git_repository_free}, prefix{prefix_}{
         walker = RevWalker(*this);
+        populate();
+      }
+
+      void populate()
+      {
+        std::println("Searching for commits with marker \"{}\"", prefix);
+
+        auto add_to_commits = [this](git_oid oid)
+        {
+          Commit c{*this, oid};
+          println("commit:{}\nmessage:{}", c.sha, c.message);
+          if (c.message.starts_with(prefix))
+          {
+            this->marked_commits.emplace_back(std::move(c));
+          }
+        };
+
+        detail::walk_commit_oids(walker.get(), repo.get(), add_to_commits);
       }
 
       void listCommits(){
-        auto c = Commit(*this, walker);
-        std::println("{}\n{}", c.sha, c.message);
+        // auto c = Commit(*this, walker);
+        // std::println("{}\n{}", c.sha, c.message);
       };
+
+      git_repository* get() { return repo.get();}
     };
 
-    static Repo& gimmeRepo(std::string_view repoPath){
+    static Repo& GimmeRepo(std::string_view repoPath, std::string_view prefix = ""){
       static Git git{};
       static std::unordered_map<std::string_view, Repo> repos{};
 
       if (!repos.contains(repoPath))
       {
-        repos.insert(std::make_pair(repoPath, Repo{repoPath}));
+        repos.insert(std::make_pair(repoPath, Repo{repoPath, prefix}));
       }
       return repos.at(repoPath);
     }
